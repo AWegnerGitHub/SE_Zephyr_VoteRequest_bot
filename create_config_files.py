4b19929d24080ab2ccf559e7cde6d918092e4237
ProgramFOX <programfox@hotmail.be>
2015-04-18 15:13:18 +0200

    Remove room 69389: Clean up "looking for"
    The 'Clean up "looking for"' room has been frozen, so Zephyr shouldn't watch it anymore.

--- create_config_files.py
+++ create_config_files.py
@@ -173,12 +173,6 @@ rooms = [
         'post_requests': False
     },
     {
-        'site': "stackoverflow.com",
-        'room_num': 69389,
-        'monitor': True,
-        'post_requests': False
-    },
-    {
         'site': "meta.stackexchange.com",
         'room_num': 773,
         'monitor': False,
